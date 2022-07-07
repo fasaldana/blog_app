7989aa4580f853ce29c25e09e9b25e05c7ad9975
fasaldana <fernando_s_98@hotmail.com>
2022-07-07 13:56:49 -0500

    fix references with correct names

--- db/migrate/20220707172003_add_user_ref_to_likes.rb
+++ db/migrate/20220707172003_add_user_ref_to_likes.rb
@@ -1,5 +1,5 @@
 class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :user, foreign_key: true
+    add_reference :likes, :author, foreign_key: { to_table: :users }
   end
 end
