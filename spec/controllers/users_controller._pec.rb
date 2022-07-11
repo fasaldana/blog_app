558620f69efa98711e948279a380c65d599005a5
fasaldana <fernando_s_98@hotmail.com>
2022-07-11 15:09:31 -0500

    test setup controllers tests

---
+++ spec/controllers/users_controller._pec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'returns http success' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+
+    it 'renders the correct text on template' do
+      expect(response.body).to include('User show page')
+    end
+  end
+end
