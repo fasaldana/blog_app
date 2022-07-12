03baeb2f027a9f697affdff52c2e5bf8159da59a
fasaldana <fernando_s_98@hotmail.com>
2022-07-12 16:19:50 -0500

    fix names and types

--- spec/requests/posts_controllers_spec.rb
+++ spec/requests/posts_controllers_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
+RSpec.describe PostsController, type: :request do
   describe 'GET #index' do
     it 'returns http success' do
       get :index
--- spec/requests/users_controller._pec.rb
+++ spec/requests/users_controller._pec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe UsersController, type: :request do
   describe 'GET #index' do
     it 'returns http success' do
       get :index
