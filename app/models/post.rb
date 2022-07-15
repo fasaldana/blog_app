729faf9744540d23f55ad046d1d903c1d6c30fcb
fasaldana <fernando_s_98@hotmail.com>
2022-07-15 12:51:09 -0500

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,15 +7,14 @@ class Post < ApplicationRecord
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_post_count
-  
+
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
-  
+
   private
 
   def update_post_count
     author.update(posts_counter: author.posts.count)
   end
-
 end
