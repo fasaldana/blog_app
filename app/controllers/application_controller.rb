03bb82aff7b0d96e780326279a137da1e6c8eb44
fasaldana <fernando_s_98@hotmail.com>
2022-07-21 14:31:00 -0500

    Add devise configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user = User.first
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  def print_current_user
+    p current_user
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :email, :password, :current_password)
+    end
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+
+    else
+      redirect_to new_user_session_path, notice: 'Please sign in'
+    end
   end
 end
